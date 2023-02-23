97c1ae938c551f481d17e1bec16a294157922a3e
Kenya Womack <ladykamillion27@gmail.com>
2023-02-22 22:54:19 -0600

    update for loop

--- script.js
+++ script.js
@@ -1,25 +1,64 @@
-let myName = "Kenya";
+function greeting() {
+    let userName = prompt("What is your name?");
+    console.log(userName);
+
+    alert("Hello," + userName);
+
+    document.write("Hello to you " + userName);
+}
+
+function validateForm(answer) {
+    if (answer === "") {
+        alert(answer + " must be filled out");
+        console.log(answer);
+        return false;
+
+    }
+}
+
+greeting();
+
+// let myName = "Kenya";
 alert("Welcome to my page!");
 
 
-let userName = prompt("What is your name?");
-console.log(userName);
-
-alert("Hello," + userName);
-
-document.write("Hello to you!");
 
 
-let animeChoice = prompt("Do you like One Piece? Type yes or no");
-console.log(animeChoice);
-
-if(animeChoice == "yes"){
-    document.write("I do too!");
-} else if (animeChoice == "no"){
-  let anime =  prompt("What anime do you like to watch?");
-    document.write("I like " + anime + " too.");
-}else {
-    document.write("I'll have to check that one out!");
+function likesAnime() {
+    let animeChoice = prompt("Do you like One Piece? Type yes or no");
+    console.log(animeChoice);
+    validateForm(animeChoice)
+    if (animeChoice == "yes") {
+        document.write("I do too!");
+    } else if (animeChoice == "no") {
+        let anime = prompt("What anime do you like to watch?");
+        document.write("I like " + anime + " too.");
+    } else {
+        document.write("I'm not sure what that one is. I'll have to check that one out!");
+    }
 }
 
-document.write("Thank you for visiting my page!");
+likesAnime(),
+    ending();
+
+function ending() {
+    document.write("Thank you for visiting my page!");
+}
+
+let myNumber = prompt("How many stars would you give One Piece? 1-5");
+
+
+for (let i = 1; i <= myNumber; i++) {
+    let img = document.createElement("img");
+    img.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRRpnDIQumZ4M76KIk21xmtrk0pDPIOXLzKbA&usqp=CAU";
+    let image = document.getElementById("picture");
+    image.appendChild(img);
+
+}
+
+let x = 5
+
+while (x < 5) {
+    console.log(x)
+    x++;
+}
